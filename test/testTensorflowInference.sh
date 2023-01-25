7ac645dd362001bd8106996e654a561ded77586e
Fabian Orland <orland@itc.rwth-aachen.de>
2023-01-25 14:08:58 +0100

    quick and dirty CI job scripts for tensorflowInference test

--- test/testTensorflowInference.sh
+++ test/testTensorflowInference.sh
@@ -3,8 +3,8 @@
 #SBATCH --ntasks=48
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=3G
-#SBATCH --job-name=CI-testTorchInference
-####SBATCH --output=CI-testTorchInference.%J.out # do not set for CI
+#SBATCH --job-name=CI-testTensorflowInference
+####SBATCH --output=CI-testTensorflowInference.%J.out # do not set for CI
 #SBATCH --account=rwth0792
 #SBATCH --time=00:10:00
 #SBATCH --partition=c18g
@@ -25,9 +25,9 @@ pwd
 
 cd /home/rwth0792/aixeleratorservice
 
-rm -rf BUILD-CI-PT
-mkdir BUILD-CI-PT && cd BUILD-CI-PT
+rm -rf BUILD-CI-TF
+mkdir BUILD-CI-TF && cd BUILD-CI-TF
 
 Torch_DIR=/home/rwth0792/AI-Frameworks/torch/libtorch-1.10.0-cuda-11.3/ cmake -DTensorflow_DIR=/home/rwth0792/AI-Frameworks/libtensorflow-gpu-linux-x86_64-2.6.0 .. && \
 cmake --build . && \
-./test/testTorchInference.cpp.x
+./test/testTensorflowInference.cpp.x
